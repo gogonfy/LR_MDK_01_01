a6f08e71c83a0b234f7226e43e4f1fd27fa5bdb2
Gogonfy <ivanpetrov220022@gmail.com>
2026-09-19 12:51:15 +0300

    Задача 1

---
+++ Console/Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Write("Сколько чисел ввести? n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] numbers = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите число {i + 1}: ");
+            
+                numbers[i] = double.Parse(Console.ReadLine());
+                
+                
+            }
+
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] % 2 == 0) 
+                evenCount++;
+                    else oddCount++;
+            }
+            Console.WriteLine($"\nЧётных: {evenCount}");
+            Console.WriteLine($"Нечётных: {oddCount}");
+        }
+    }
+}
